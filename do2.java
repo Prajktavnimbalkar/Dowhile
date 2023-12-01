690be228bd6c6444c6e10257acae8839c60bb676
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-12-01 09:10:19 +0530

    Add files via upload

---
+++ do2.java
@@ -0,0 +1,17 @@
+package dowhile;
+
+public class do2 {
+	public static void main(String[] args) {
+		if(true) {
+			do {
+				System.out.println("prajkta");
+			}
+			while(1>2);
+		}else
+		{
+			System.out.println("insufficient balance...");
+		}
+	}
+	
+
+}
