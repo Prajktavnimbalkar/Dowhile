fdfbfb8e4a81b77b30d9bd78afb7bda336cda1e7
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-12-12 08:59:58 +0530

    Add files via upload

---
+++ oneto100.java
@@ -0,0 +1,15 @@
+package dowhile;
+
+public class oneto100 {
+	public static void main(String[] args) {
+		int i = 1;
+		do { 
+			System.out.println(i);
+			i++;
+		}
+			while(i<=100);
+ }
+
+}
+
+
